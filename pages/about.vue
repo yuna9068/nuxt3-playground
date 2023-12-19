a104282c88a1018c85cfc89644d3bf92c7ef3fae
Yuna <yuna9068@gmail.com>
2023-12-19 21:15:34 +0800

    feat: 新增 page - index, about

---
+++ pages/about.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Page: About</h1>
+    <NuxtLink to="/">Index</NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Page: Index</h1>
+    <NuxtLink to="about">About</NuxtLink>
+  </div>
+</template>
+
+<style scoped></style>
