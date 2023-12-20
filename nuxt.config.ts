d417c99b97ad26af7a716781f733f526c4824574
Yuna <yuna9068@gmail.com>
2023-12-20 14:04:40 +0800

    feat: 修改 config path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,36 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+const path = process.env.NODE_ENV === 'development' ? '/' : '/nuxt3-playground/'
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
-    baseURL: process.env.NODE_ENV === 'development' ? '/' : '/nuxt3-playground/',
+    baseURL: path,
     buildAssetsDir: '/static/',
     head: {
       link: [
         {
           rel: 'icon',
           type: 'image/png',
-          href: './favicon.ico',
+          href: `${path}favicon.ico`,
         },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '32x32',
-          href: './favicon-32x32.png',
+          href: `${path}favicon-32x32.png`,
         },
         {
           rel: 'icon',
           type: 'image/png',
           sizes: '16x16',
-          href: './favicon-16x16.png',
+          href: `${path}favicon-16x16.png`,
         },
         {
           rel: 'apple-touch-icon',
           sizes: '180x180',
-          href: './apple-touch-icon.png',
+          href: `${path}apple-touch-icon.png`,
         },
         {
           rel: 'manifest',
-          href: './manifest.json',
+          href: `${path}manifest.json`,
         },
       ],
     }
