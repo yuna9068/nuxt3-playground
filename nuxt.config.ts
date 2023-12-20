2d759b7c91f8428524b6e2558a14f0a101c126d3
Yuna <yuna9068@gmail.com>
2023-12-20 13:53:49 +0800

    feat: 新增 images, reset css, favicon, manifest.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,38 @@ export default defineNuxtConfig({
   app: {
     baseURL: process.env.NODE_ENV === 'development' ? '/' : '/nuxt3-playground/',
     buildAssetsDir: '/static/',
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: './favicon.ico',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: './favicon-32x32.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: './favicon-16x16.png',
+        },
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: './apple-touch-icon.png',
+        },
+        {
+          rel: 'manifest',
+          href: './manifest.json',
+        },
+      ],
+    }
   },
+  css: [
+    '~/assets/style/reset.css',
+  ],
 })
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,14 @@
   <div>
     <h1>Page: Index</h1>
     <NuxtLink to="about">About</NuxtLink>
+    <section class="section-pic">
+      <img class="pic" src="@/assets/images/main.png" alt="main">
+    </section>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.pic {
+  height: 200px;
+}
+</style>
